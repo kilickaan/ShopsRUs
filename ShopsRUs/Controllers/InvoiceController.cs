8e38bd0638bfc13a09e8d66efe8dbb8216d2af6e
Kaan KILIÇ <slothico@gmail.com>
2022-03-12 00:41:57 +0300

    dotnet new wepapi -n ShopsRUs

---
+++ ShopsRUs/Controllers/InvoiceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopsRUs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InvoiceController : ControllerBase
+    {
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateInvoice()
+        {
+
+
+            return Ok("Invoice Has Been Created");
+        }
+    }
+}
